4145a6e1f2623e8a7822f1cfbe8cc2424cd5f3a0
Ahmet <parisstar033@gmail.com>
2020-08-16 22:07:47 +0200

    feature

--- src/git_project_sonders/SonDersTekrar.java
+++ src/git_project_sonders/SonDersTekrar.java
@@ -4,9 +4,9 @@ public class SonDersTekrar {
 
 	public static void main(String[] args) {
 		
-		System.out.println("21:55 master branch");
+		System.out.println("21:55- master branch");
 		
-		
+		System.out.println("22:05- feature branch");
 
 	}
 
